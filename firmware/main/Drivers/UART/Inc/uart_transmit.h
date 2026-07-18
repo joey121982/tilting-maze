3c8f758b33b4a0991bfc98c279eb5241d562a617
cosminbrn <cosminbaroana06@gmail.com>
2026-07-18 11:09:47 +0300

    feat: start work on UART driver

--- firmware/main/Drivers/UART/Inc/uart_transmit.h
+++ firmware/main/Drivers/UART/Inc/uart_transmit.h
@@ -16,7 +16,7 @@ namespace UART {
     /** @brief UART port definition class */
     class UART_PORT {
     private:
-        UART_HandleTypeDef _huart;
+        UART_HandleTypeDef _huart; // in stm32f1xx_hal_uart.h, same with its Init struct
 
         GPIO_TypeDef* _tx_port;
         uint16_t _tx_pin;
@@ -26,17 +26,22 @@ namespace UART {
 
         uint32_t _baud_rate;
 
+        /** @brief Enables the peripheral clocks and configures the TX and RX pins */
         bool _setupHardwareInstance();
 
     public:
-        // TODO: add missing doxygen comments
 
-        UART_PORT(GPIO_TypeDef* tx_port, uint16_t tx_pin, 
-                  GPIO_TypeDef* rx_port, uint16_t rx_pin, 
+        UART_PORT(GPIO_TypeDef* tx_port, uint16_t tx_pin,
+                  GPIO_TypeDef* rx_port, uint16_t rx_pin,
                   uint32_t baud = 115200);
 
+        /**
+         * @brief Configures the clocks, the pins and the USART1. Uses 8N1 settings.
+         *
+         * @return true if HAL_UART_Init() returned HAL_OK, false otherwise
+         */
         bool init();
-        
+
         void setBaudRate(uint32_t baud);
         uint32_t getBaudRate() const;
         UART_HandleTypeDef* getHandle();
@@ -48,7 +53,7 @@ namespace UART {
         UART_PORT _port;
 
     public:
-        // TODO: add missing doxygen comments
+
 
         UART_DEVICE (
             GPIO_TypeDef* tx_port, uint16_t tx_pin, 
@@ -57,32 +62,29 @@ namespace UART {
         ) : _port(tx_port, tx_pin, rx_port, rx_pin, baud) {};
 
         /**
-         * @brief 
+         * @brief Initializes the UART device
          * 
-         * @return true 
-         * @return false 
+         * @return true if port succeeded, false otherwise
          */
         bool init() { return _port.init(); };
 
         /**
-         * @brief 
+         * @brief Prints a message to the UART device. INTRERUPT mode is not finished
          * 
-         * @param msg 
-         * @param mode 
+         * @param msg pointer to a string, the message to be printed
+         * @param mode the mode of transmission, either BLOCKING (default, CPU will wait until the message is fully transmitted) or INTERRUPT (CPU will not wait for the message to be fully transmitted)
          * 
-         * @return true 
-         * @return false 
+         * @return true if HAL transmit functions succeeded, false otherwise
          */
         bool print(const char* msg, TRANSMIT_MODE mode = BLOCKING);
 
         /**
-         * @brief 
+         * @brief Prints a message to the UART device, followed by a newline character. INTERRUPT mode is not finished
          * 
-         * @param msg 
-         * @param mode 
+         * @param msg pointer to a string, the message to be printed
+         * @param mode the mode of transmission, either BLOCKING (default, CPU will wait until the message is fully transmitted) or INTERRUPT (CPU will not wait for the message to be fully transmitted)
          * 
-         * @return true 
-         * @return false 
+         * @return true if HAL transmit functions succeeded, false otherwise
          */
         bool println(const char* msg, TRANSMIT_MODE mode = BLOCKING);
 
